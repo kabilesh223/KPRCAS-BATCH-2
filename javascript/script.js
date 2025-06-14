3d37f3969ad11cfb85a8e1df22f251f911bebdf7
kabiles <kabilesvarsan@mail.com>
2025-06-14 15:56:51 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,97 @@
+// function sum(a,b){
+//     console.log(a+b);
+    
+// }
+// sum(10,20)
+
+
+// var sum =()=>{
+//     var a=10;
+//     var b=20;
+//     console.log(a+b);
+    
+    
+//     }
+// sum(10+20)
+                 
+
+
+
+
+
+
+
+// let arr = [10,29,39,49]
+// let arr2 = [...arr,50,60,70,80,]
+// console.log(arr);
+// console.log(arr2);
+
+var mark =[99,90,98,96,100]
+var [m1,m2,m3,m4,m5,] =mark;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a="10";
+// var b=10;
+
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+
+// var mark=90;
+//  var result =(mark>=80)? "A":(mark>=60)?"A+":(mark>=35 )?"p":"f"
+//  console.log(result);
+ 
+
+// if(mark>=89)
+// {
+//  console.log("0");
+ 
+// }
+// elseif(mark>=60)
+// {
+//     console.log("A+");
+    
+// }
+// elseif(mark>=35)
+// {
+//     console.log("p");
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
